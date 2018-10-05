8eb8e05ea7079243c5fe0ec00f0788e516133b08
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-05 16:19:02 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if name.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
