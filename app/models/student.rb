4d6fbc8d525175d4e086fe353bb555120501bbd7
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-05 16:06:37 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if name.present?
+      where('NAME like ?', "%#{query}")
+    else 
+      self.all
+  end
 end
